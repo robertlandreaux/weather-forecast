94a453028d64702aa58adab91701609e653aa6c3
Robert Landreaux <robertlandreaux@gmail.com>
2025-09-09 07:53:43 -0400

    Use default for redis-client connection pool size

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.strict_args!
 
 Sidekiq.configure_server do |config|
-  config.redis = {url: ENV["REDIS_URL"], size: 5}
+  config.redis = {url: ENV["REDIS_URL"]}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: ENV["REDIS_URL"], size: 1}
+  config.redis = {url: ENV["REDIS_URL"]}
 end
