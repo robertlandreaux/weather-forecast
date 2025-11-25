bc2bfc53542df47819a49d42d362dfe11bb208c2
Robert Landreaux <robertlandreaux@gmail.com>
2025-11-25 08:44:25 -0500

    Install rswag

---
+++ spec/requests/api/locations_spec.rb
@@ -0,0 +1,21 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/locations', type: :request do
+
+  path '/api/locations' do
+
+    post('create location') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
