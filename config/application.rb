1d91e75a9e75feec834e888eb6ebf4399b035682
Robert Landreaux <robertlandreaux@gmail.com>
2025-12-01 09:33:10 -0500

    Add ability to bulk enqueue mailer jobs

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module WeatherForecast
 
     config.active_job.queue_adapter = :sidekiq
 
+    config.action_mailer.deliver_later_queue_name = :mailers
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
---
+++ spec/services/bulk_enqueue_mailers_spec.rb
@@ -0,0 +1,62 @@
+require "rails_helper"
+
+RSpec.describe BulkEnqueueMailers, type: :job do
+  include_context "with application data"
+
+  let(:bulk_enqueuer) { described_class.new }
+  let(:mail_class) { ForecastMailer }
+  let(:template) { :new_forecast }
+  let(:args_array) {
+    [
+      [forecast_1, user_1],
+      [forecast_2, user_2]
+    ]
+  }
+
+  let(:user_1) { TestProf::AnyFixture.cached(:user_1) }
+  let(:user_2) { TestProf::AnyFixture.cached(:user_2) }
+  let(:forecast_1) { TestProf::AnyFixture.cached(:forecast_1_us_location) }
+  let(:forecast_2) { TestProf::AnyFixture.cached(:forecast_2_us_location) }
+
+  describe "#run" do
+    subject(:run) { bulk_enqueuer.run(mail_class, template, args_array) }
+
+    before do
+      allow(Sidekiq::Client).to receive(:push_bulk)
+    end
+
+    it "enqueues mailer jobs in bulk" do
+      run
+
+      expect(Sidekiq::Client).to have_received(:push_bulk).with(
+        "class" => ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper,
+        "wrapped" => ActionMailer::MailDeliveryJob,
+        "queue" => :mailers,
+        "args" => array_including(
+          array_including(
+            hash_including(
+              "arguments" => [
+                "ForecastMailer",
+                "new_forecast",
+                "deliver_now",
+                {"_aj_globalid" => forecast_1.to_global_id.to_s},
+                {"_aj_globalid" => user_1.to_global_id.to_s}
+              ]
+            )
+          ),
+          array_including(
+            hash_including(
+              "arguments" => [
+                "ForecastMailer",
+                "new_forecast",
+                "deliver_now",
+                {"_aj_globalid" => forecast_2.to_global_id.to_s},
+                {"_aj_globalid" => user_2.to_global_id.to_s}
+              ]
+            )
+          )
+        )
+      )
+    end
+  end
+end
