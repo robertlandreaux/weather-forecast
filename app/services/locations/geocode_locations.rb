33b5a3a6b52c3ccb6838e4b43d834f6e23dfaecc
Robert Landreaux <robertlandreaux@gmail.com>
2025-12-02 09:29:52 -0500

    Add scripts

---
+++ app/services/locations/geocode_locations.rb
@@ -0,0 +1,9 @@
+module Locations
+  class GeocodeLocations
+    def run
+      Location.where(latitude: nil, longitude: nil).find_each do |location|
+        Locations::GeocodeLocation.new(location_id: location.id).run
+      end
+    end
+  end
+end
