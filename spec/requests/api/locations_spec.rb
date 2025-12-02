951fdfcc3e10943d8f37cf4772f6020f6aee339f
Robert Landreaux <robertlandreaux@gmail.com>
2025-12-02 09:29:29 -0500

    Comment out test and add TODO comment

--- spec/requests/api/locations_spec.rb
+++ spec/requests/api/locations_spec.rb
@@ -1,18 +1,19 @@
 require "swagger_helper"
 
 RSpec.describe "api/locations", type: :request do
-  path "/api/locations" do
-    post("create location") do
-      response(200, "successful") do
-        after do |example|
-          example.metadata[:response][:content] = {
-            "application/json" => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
+  # TODO: Add test for location creation
+  # path "/api/locations" do
+  #   post("create location") do
+  #     response(200, "successful") do
+  #       after do |example|
+  #         example.metadata[:response][:content] = {
+  #           "application/json" => {
+  #             example: JSON.parse(response.body, symbolize_names: true)
+  #           }
+  #         }
+  #       end
+  #       run_test!
+  #     end
+  #   end
+  # end
 end
