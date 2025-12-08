0404c4020078a4c03cc5b849407893ebe3139696
Robert Landreaux <robertlandreaux@gmail.com>
2025-12-08 08:27:33 -0500

    Update scripts

--- lib/scripts/geocode_locations.rb
+++ lib/scripts/geocode_locations.rb
@@ -1,7 +1,9 @@
-class GeocodeLocations
-  def run
-    Location.where(latitude: nil, longitude: nil).find_each do |location|
-      Locations::GeocodeLocation.new(location_id: location.id).run
+module Scripts
+  class GeocodeLocations
+    def run
+      Location.where(latitude: nil, longitude: nil).find_each do |location|
+        Locations::GeocodeLocation.new(location_id: location.id).run
+      end
     end
   end
 end
--- lib/scripts/set_missing_nws_attributes.rb
+++ lib/scripts/set_missing_nws_attributes.rb
@@ -1,7 +1,9 @@
-class SetMissingNwsAttributes
-  def run
-    Location.where(nws_grid_id: nil).find_each do |location|
-      Locations::SetNwsLocationAttributes.new(location_id: location.id).run
+module Scripts
+  class SetMissingNwsAttributes
+    def run
+      Location.where(nws_grid_id: nil).find_each do |location|
+        Locations::SetNwsLocationAttributes.new(location_id: location.id).run
+      end
     end
   end
 end
